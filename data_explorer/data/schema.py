153357462ed9e402fd5f85a604a648bc012a6d25
Kylie <kyliehumble@yahoo.com>
2020-03-03 17:40:01 -0600

    updated functions

--- data_explorer/data/schema.py
+++ data_explorer/data/schema.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 class Schema:
     def __init__(self, num_of_columns, rules):
         self.num_of_columns = num_of_columns
@@ -31,16 +29,6 @@ class Schema:
         else:
             return True
 
-    # def check_dataframe_old(self):
-    #     if self.check_num_of_columns:
-    #         for self.rule in self.rules:
-    #             self.name = self.rule['name']
-    #             self.type = self.rule['type']
-    #             if self.check_type:
-    #                 greater_equal = self.rule['greater_equal']
-    #                 if not (self.dataframe[greater_equal] >= greater_equal).all():
-    #                     return False
-
     def check_dataframe(self, dataframe):
         num_dataframe_cols = dataframe.shape[1]
         if self.check_num_of_columns(num_dataframe_cols):
@@ -56,50 +44,3 @@ class Schema:
             else:
                 return False
         return True
-
-        #dataframe_columns = self.dataframe.columns
-        #for df_col in dataframe_columns:
-            #1. check data_type
-        #    if not self.check_type:
-        #        pass
-            #2. check additional rules
-        #    if not self.check_greater_equal: # complete conditions
-        #        pass
-
-    # # def check_dataframe(self, dataframe): # restructure func. into multi. smaller funcs.
-    #     # 1. check if number of columns in dataframe == self.num_of_columns
-    #     if self.num_of_columns == self.dataframe.shape[1]:
-    #     #   1.1. if true, iterate through rules
-    #         for rule in self.rules:
-    #     #       1.1.1. for each rule, check that type matches dataframe column type
-    #     #               e.g. dataframe[<name>].dtype == <type>
-    #             name = rule['name']
-    #             type = rule['type']
-    #             if self.dataframe[name].dtype == type:
-    #         #       1.1.2. if rule contains "greater_equal" key, check that all values
-    #                 if 'greater_equal' in rule.keys():
-    #                     greater_equal = rule['greater_equal']
-    #                     if not (self.dataframe[greater_equal] >= greater_equal).all():
-    #                         return False
-    #             else:
-    #                 return False
-    #         return True
-    #     else:
-    #         return False
-    #
-    #
-    #     #               in dataframe column are greater than or equal to the
-    #     #               given value
-    #     #               e.g. (dataframe[<name>] >= <value>).all()
-    #     #       1.1.3. if either of the above is false, return False
-    #     #               otherwise, return true
-    #     #   1.2. if false, return False
-    #     # take list of types and process data from list
-    #     dataframe_columns = self.dataframe.columns
-    #     for df_col in dataframe_columns:
-    #         #1. check data_type
-    #         if not self.check_type():
-    #             pass
-    #         #2. check additional rules
-    #         if not self.check_greater_equal(): # complete conditions
-    #             pass
--- data_explorer/tests/seed/test_schema.py
+++ data_explorer/tests/seed/test_schema.py
@@ -1,6 +1,7 @@
 from data_explorer.data.schema import Schema
 import pandas as pd
 from data_explorer.data.seed import load_schema
+import pytest
 #import json
 
 # with open('./seed/schemas/test_schema.json', 'r') as json_file:
@@ -31,6 +32,10 @@ def test_check_dataframe(): # complete test_schema
     # assert result is True
     assert ch_df is True
 
+    test_df['firm'] = ['1', '2', '3', '4']
+    ch_df = test_schema.check_dataframe(test_df)
+    assert ch_df is False
+
 def test_check_greater_equal():
     test_schema, test_df = setup()
     ch_ge = test_schema.check_greater_equal(test_schema.rules[2], test_df)
@@ -40,7 +45,32 @@ def test_check_greater_equal():
     ch_ge = test_schema.check_greater_equal(test_schema.rules[2], test_df)
     assert ch_ge is False
 
-def test_check_type():
+@pytest.mark.parametrize("rule,  result", [
+    pytest.param(dict(name='entrsize', type='object'), True),
+    pytest.param(dict(name='entrsize', type='int64'), False),
+    pytest.param(dict(name='firm', type='int64'), True)])
+
+def test_check_type(rule, result):
     test_schema, test_df = setup()
-    ch_t = test_schema.check_type(test_schema.rules[1], test_df)
-    assert ch_t is True
+    ch_t = test_schema.check_type(rule, test_df)
+    assert ch_t is result
+
+@pytest.mark.parametrize("num_cols,  result", [
+    pytest.param(3, True),
+    pytest.param(4, False)])
+
+def check_num_of_columns(num_cols, result):
+    test_schema, test_df = setup()
+    ch_c = test_schema.check_num_of_columns(num_cols)
+    assert ch_c is result
+
+@pytest.mark.parametrize("col, col_value,  result", [
+    pytest.param("payr", [-2, -1, 0, 1], False),
+    pytest.param("firm", ['1', '2', '3', '4'], False),
+    pytest.param("firm", [1, 2, 3, 4], True)])
+
+def test_check_dataframe_columns(col, col_value, result):
+    test_schema, test_df = setup()
+    test_df[col] = col_value
+    ch_t = test_schema.check_dataframe_columns(test_df)
+    assert ch_t is result
